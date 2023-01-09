52c9af3938a1a5d16a7357b0fe70c17ab2fcb3b7
nizaralloul7 <nizaralloul7@outlook.fr>
2023-01-09 21:29:46 +0100

    heroku

--- src/main/java/com/example/apigateway/ApiGatewayApplication.java
+++ src/main/java/com/example/apigateway/ApiGatewayApplication.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class ApiGatewayApplication
 {
+    //test
     public static void main(String[] args) {
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
